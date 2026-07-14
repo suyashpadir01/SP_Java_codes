c612c6521cfcd9af882f6e3a9596f34d5021340b
Suyash Padir <suyashpadir01@gmail.com>
2026-07-14 22:25:38 +0530

    .

---
+++ Problems/StudentInfo.java
@@ -0,0 +1,8 @@
+class StudentInfo {
+    public static void main(String args[]) {
+        System.out.println("Name : Suyash Padir");
+        System.out.println("Roll No : 101");
+        System.out.println("Class : FY CSBS");
+        System.out.println("College : JSPM RSCOE");
+    }
+}
